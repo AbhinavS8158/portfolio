81752fdb13ce0d1c001b6ed7fed94f8b20603925
AbhinavS8158 <abhinav.8158s@gmail.com>
2026-06-27 00:04:59 +0530

    Manual deploy to gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2194025997" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3161402095" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
